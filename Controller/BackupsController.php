d09505aed0c9f45eeb89364778c90c2076f6a9a4
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-26 11:27:53 +0100

    logic added

--- Controller/BackupsController.php
+++ Controller/BackupsController.php
@@ -5,12 +5,38 @@ class BackupsController extends ApiController
     public function Get(): array
     {
 
-        return [];
+        $backupFile = BackupFile::GetRecentBackupFile();
+
+        if ($backupFile == null) {
+            return ['message' => 'No backup found.'];
+        }
+
+        return $backupFile->ToArray();
     }
 
     public function Post(Request $request): array
     {
 
-        return [];
+        $platformName = $request->platform;
+
+        if ($platformName == null) {
+            return ['message' => 'platform required'];
+        }
+
+        $response = [];
+
+        $platform = Platform::GetPlatformObject($platformName);
+
+        if (property_exists($request, 'sql_dump') && $request->sql_dump) {
+
+            $response['sql_dump'] = $platform->CreateSQLDump();
+        }
+
+        if (property_exists($request, 'file_dump') && $request->file_dump) {
+
+            $response['file_dump'] = $platform->CreateFilesBackup(null);
+        }
+
+        return $response;
     }
 }
