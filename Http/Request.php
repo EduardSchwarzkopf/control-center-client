69765395f3b6e78410e4567f99ad8393f571fe03
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-26 11:28:32 +0100

    magic getter added

--- Http/Request.php
+++ Http/Request.php
@@ -2,6 +2,15 @@
 
 abstract class Request
 {
+    private $field = [];
 
     abstract public function Response(): string;
+
+    public function __get($name)
+    {
+        if (isset($this->field[$name]))
+            return $this->field[$name];
+        else
+            throw new Exception("$name dow not exists");
+    }
 }
