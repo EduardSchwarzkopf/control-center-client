83869c2cf1427ae71456d3fd14d1de9c6050615a
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-03-26 21:00:21 +0100

    use new Response class

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -2,10 +2,10 @@
 
 abstract class Controller
 {
-    abstract public function Get(): array;
-    abstract public function Post(Request $request): array;
-    abstract public function Put(Request $request): array;
-    abstract public function Delete(Request $request): array;
+    abstract public function Get(): Response;
+    abstract public function Post(Request $request): Response;
+    abstract public function Put(Request $request): Response;
+    abstract public function Delete(Request $request): Response;
 
     static public function GetControllerByName(string $name)
     {
