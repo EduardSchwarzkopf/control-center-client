600f083fc76c8b19a53664de01a5197576deaf97
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-03-27 09:28:31 +0200

    added a request as a required prop

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -2,14 +2,21 @@
 
 abstract class Controller
 {
-    abstract public function Get(): Response;
-    abstract public function Post(Request $request): Response;
-    abstract public function Put(Request $request): Response;
-    abstract public function Delete(Request $request): Response;
+    protected Request $request;
 
-    static public function GetControllerByName(string $name)
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    abstract public function Get(): Response;
+    abstract public function Post(): Response;
+    abstract public function Put(): Response;
+    abstract public function Delete(): Response;
+
+    static public function GetControllerByName(string $name, Request $request)
     {
         $classname = $name . 'Controller';
-        return ClassUtils::GetClassByName($classname);
+        return ClassUtils::GetClassByName($classname, [$request]);
     }
 }
