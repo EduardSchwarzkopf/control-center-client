e971b58c4444fdb2f48d9a6c895c5b04eb5608a9
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-03-27 09:13:24 +0200

    create dynamic object with dynamic props

--- Utils/ClassUtils.php
+++ Utils/ClassUtils.php
@@ -4,12 +4,13 @@
 class ClassUtils
 {
 
-    static public function GetClassByName(string $classname): ?object
+    static public function GetClassByName(string $classname, array $properties = []): ?object
     {
 
         $object = null;
         try {
-            $object = new $classname;
+
+            $object = new $classname(...$properties);
         } catch (Exception $e) {
             Logger::Error($e);
         }
