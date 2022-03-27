fd44c8e555ac71f96d55e6108b76d3b7f763340e
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-03-27 13:02:44 +0200

    always return a fileobject

--- File.php
+++ File.php
@@ -11,21 +11,21 @@ class File
     private int $bytes = 0;
     private int $hours = 0;
 
-    function __construct(string $location)
+    function __construct(string $location = '')
     {
+
+        $this->exist = file_exists($location);
+        if ($this->exist == false) {
+            return $this;
+        }
 
         $this->absolute_location = $location;
         $this->relative_location = str_replace(CLIENT_ROOT  . '/', '', $location);
-
-        $this->exist = file_exists($location);
-
-        if ($this->exist) {
-            $this->bytes = filesize($location);
-            $this->human_size = FileUtils::HumanFileSize($this->bytes);
-            $this->date = FileUtils::GetModificationDate($location);
-            $this->hours = FileUtils::GetAgeHours($this->date);
-            $this->filetime = filemtime($location);
-        }
+        $this->bytes = filesize($location);
+        $this->human_size = FileUtils::HumanFileSize($this->bytes);
+        $this->date = FileUtils::GetModificationDate($location);
+        $this->hours = FileUtils::GetAgeHours($this->date);
+        $this->filetime = filemtime($location);
     }
 
     public function Location(): string
@@ -65,6 +65,13 @@ class File
 
     public function ToArray()
     {
-        return get_object_vars($this);
+        if ($this->exist == false) {
+            return [];
+        }
+
+        $vars = get_object_vars($this);
+        unset($vars['exist']);
+
+        return $vars;
     }
 }
