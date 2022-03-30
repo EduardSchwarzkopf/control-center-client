00690a74ea36c3fb18687b34beb79696ff2658a3
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-30 17:22:13 +0200

    cleaner magento version

--- Platforms/Magento2Platform.php
+++ Platforms/Magento2Platform.php
@@ -1,5 +1,6 @@
 <?php
 
+use Magento\Framework\App\ObjectManager;
 
 class Magento2Platform extends PhpPlatform
 {
@@ -8,12 +9,16 @@ class Magento2Platform extends PhpPlatform
     {
 
         parent::__construct();
-        $configList = include($this->platformRoot . '/app/etc/env.php');
+        $root = $this->platformRoot;
+        $configList = include($root . '/app/etc/env.php');
 
         $credentials = $configList["db"]['connection']['default'];
         $this->host = $credentials["host"];
         $this->database = $credentials["dbname"];
         $this->username = $credentials["username"];
         $this->password = $credentials["password"];
+
+        $versionOutput = exec("php $root/bin/magento --version");
+        $this->version = str_replace('Magento CLI ', '', $versionOutput);
     }
 }
